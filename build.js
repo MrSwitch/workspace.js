dff1548e9ce9a0da887f65edc60e03f7738aed71
Andrew Dodson <sonofdod@gmail.com>
2015-09-05 18:52:39 +0100

    Update build to create a minified workspace

--- build.js
+++ build.js
@@ -2,6 +2,7 @@
 // Package the files up
 //
 require('shunt')({
+	"./dist/workspace.min.js" : ["./dist/workspace.js"],
 	"./dist/workspace.css" : ["./src/workspace.css"],
 	"./README.md"	: ["./index.html"]
 });
