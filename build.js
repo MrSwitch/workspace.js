38d274d5e239773bdfee70f78f6773ac40913979
Andrew Dodson <sonofdod@gmail.com>
2015-09-05 17:19:50 +0100

    Shunt move CSS files

--- build.js
+++ build.js
@@ -2,7 +2,6 @@
 // Package the files up
 //
 require('shunt')({
-	"./dist/workspace.js" : ["./src/workspace.js","./src/jquery.touch.js"],
 	"./dist/workspace.css" : ["./src/workspace.css"],
 	"./README.md"	: ["./index.html"]
 });
